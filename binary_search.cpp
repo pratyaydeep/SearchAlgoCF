5d8f1e0c1f2a4759722c873152e6b496265f7e6d
Sayan Bose <48182104+SBose999@users.noreply.github.com>
2020-10-01 21:36:42 +0530

    Updated with iterative Binary Search

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,10 +1,27 @@
 #include<iostream>
 using namespace std;
 
-int binary_search(vector<int>& arr){
+bool binary_search(vector<int>& arr, int key){
   //complete this function
- }
+  int low = 0, hi = arr.size();
+  while(low < hi) {
+    int mid = low + (hi - low) / 2;
+    if(arr[mid] == key) {
+      return true;
+    }
+    else if (key >= arr[mid]) {
+      low = mid + 1;
+    }
+    else 
+      hi = mid - 1;
+  }
+  return false;
+}
  
- int main()
- {
- }
+int main() {
+  // INPUT
+  
+  // PROCESS
+  
+  return 0;
+}
